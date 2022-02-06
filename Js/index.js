f3f839db5d4dc4251f7b8346c9a478b8bd4da2e2
Rony Parmar <ronyparmar2107@gmail.com>
2022-02-06 12:35:04 +0530

    Updated the SwiperJs Library

--- Js/index.js
+++ Js/index.js
@@ -1,23 +1,22 @@
-const swiper = new Swiper('.swiper-container', {
-    
-    
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      dynamicBullets: true,
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+const swiper = new Swiper('.mySwiper', {
 
 
- 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+
